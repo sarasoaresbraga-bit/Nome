069ceec2337bce75412e98bfdacdadf35c79fa53
sarasoaresbraga-bit <sara.soares.braga@escola.pr.gov.br>
2026-05-29 11:14:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Função do botão "Saiba Mais"
+document.getElementById('btnSaibaMais').addEventListener('click', function() {
+    alert('O Agrinho Paraná 2026 incentiva cidadania, educação e sustentabilidade nas escolas do estado!');
+});
 
+// Validação simples do formulário
+document.getElementById('formContato').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Mensagem enviada com sucesso! Obrigado pelo contato.');
+    this.reset();
+});
